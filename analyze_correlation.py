b09cd880b04cc87a431c5b0f4c6a30b1b037d64b
Jiwon Park <jiwonpark81@gmail.com>
2020-08-22 15:39:18 +0900

    봉 데이터 순서 헷깔리니까 잘 명명된 변수에 할당

--- analyze_correlation.py
+++ analyze_correlation.py
@@ -4,7 +4,13 @@ from datetime import datetime
 segment = retrieve_polo('USDT_LTC', 30, '2020-08-10', '2020-08-16') # 요구된 봉 데이터를 Poloniex 거래소 API로 읽어와서 n x 6 크기의 numpy array로 만들어주는 함수
 
 for i in range(segment.shape[0]):
-    print('{:s} {:.3f} {:.3f} {:.3f} {:.3f} {:10.3f}'.format(str(datetime.fromtimestamp(segment[i,0])), segment[i,1], segment[i,2], segment[i,3], segment[i,4], segment[i,5]))
+    ts = segment[i,0]
+    open = segment[i,1]
+    close = segment[i,2]
+    high = segment[i,3]
+    low = segment[i,4]
+    volume = segment[i,5]
+    print('{:s} {:.3f} {:.3f} {:.3f} {:.3f} {:10.3f}'.format(str(datetime.fromtimestamp(ts)), open, close, high, low, volume))
 
 print('TIMESTAMP           OPEN   CLOSE  HIGH   LOW    VOLUME')
 
