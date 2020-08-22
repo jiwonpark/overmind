ec0737c046ba8e176774b005baec298d207c0bbe
Jiwon Park <jiwonpark81@gmail.com>
2020-08-22 18:15:13 +0900

    1% 이상 오른 봉들만 뽑아보기

--- analyze_correlation.py
+++ analyze_correlation.py
@@ -3,6 +3,8 @@ from datetime import datetime
 
 segment = retrieve_polo('USDT_LTC', 30, '2020-08-10', '2020-08-16') # 요구된 봉 데이터를 Poloniex 거래소 API로 읽어와서 n x 6 크기의 numpy array로 만들어주는 함수
 
+purge_percent_threshold = 1
+
 for i in range(segment.shape[0]):
     ts = segment[i,0]
     open = segment[i,1]
@@ -10,9 +12,11 @@ for i in range(segment.shape[0]):
     high = segment[i,3]
     low = segment[i,4]
     volume = segment[i,5]
-    print('{:s} {:.3f} {:.3f} {:.3f} {:.3f} {:10.3f}'.format(str(datetime.fromtimestamp(ts)), open, close, high, low, volume))
+    movement_percent = (close / open - 1) * 100
+    if movement_percent > purge_percent_threshold:
+        print('{:s} {:.3f} {:.3f} {:.3f} {:.3f} {:10.3f} {:6.2f}%'.format(str(datetime.fromtimestamp(ts)), open, close, high, low, volume, movement_percent))
 
-print('TIMESTAMP           OPEN   CLOSE  HIGH   LOW    VOLUME')
+print('TIMESTAMP           OPEN   CLOSE  HIGH   LOW    VOLUME      MOVE')
 
 print(segment.shape) # 봉 하나는 위와 같이 6개 element로 이루어져있고, 이것이 요청한 duration에 따라 n개가 있는 2차원 배열이다.
 
