82bd03943bf4a1f2f799073adf74dbf1e0d972ff
Jiwon Park <jiwonpark81@gmail.com>
2020-08-25 23:52:48 +0900

    다른 코인도 그리기 위해서 처리하는 로직을 class로 만들자

--- analyze_correlation.py
+++ analyze_correlation.py
@@ -12,8 +12,6 @@ from datetime import datetime
 def mask_h(plt, x1, x2, min_y, max_y, color, alpha):
     return plt.Rectangle((x1, min_y), x2 - x1, max_y - min_y, linewidth=0, alpha=alpha, edgecolor=color, facecolor=color)
 
-segment = retrieve_polo('USDT_LTC', 30, '2020-08-10', '2020-08-16') # 요구된 봉 데이터를 Poloniex 거래소 API로 읽어와서 n x 6 크기의 numpy array로 만들어주는 함수
-
 purge_percent_threshold = 1
 
 required_lead = 100
@@ -24,22 +22,48 @@ snapshots_count = 0
 fig = plt.figure(figsize=(20 * max_snapshots, 7))
 gs_all = gridspec.GridSpec(1, max_snapshots, figure=fig)
 
-dates = mdates.date2num([datetime.fromtimestamp(segment[i,0]) for i in range(segment.shape[0])])
-ohlc = [[dates[i], segment[i,1], segment[i,3], segment[i,4], segment[i,2]] for i in range(segment.shape[0])]
+class Series:
+    def __init__(self, segment):
+        self.segment = segment
 
-# numpy array를 pandas DataFrame으로 바꾸면 pandas에서 제공하는 보다 편리한 기능들(아래 rolling, ewm 등)을 활용할 수 있다.
-segment_pd = pd.DataFrame(data=segment[:,1:], index=dates, columns=['open', 'close', 'high', 'low', 'volume'])
+        self.dates = mdates.date2num([datetime.fromtimestamp(segment[i,0]) for i in range(segment.shape[0])])
+        self.ohlc = [[self.dates[i], segment[i,1], segment[i,3], segment[i,4], segment[i,2]] for i in range(segment.shape[0])]
 
-# pandas DataFrame은 이런식으로 index 번호가 아닌, 위에서 columns 파라미터로 지정된 label로 각 column을 지정한다.
-hsma40 = segment_pd['high'].rolling(40).mean()
-lsma40 = segment_pd['low'].rolling(40).mean()
-ema15 = segment_pd['close'].ewm(15).mean()
+        # numpy array를 pandas DataFrame으로 바꾸면 pandas에서 제공하는 보다 편리한 기능들(아래 rolling, ewm 등)을 활용할 수 있다.
+        self.segment_pd = pd.DataFrame(data=segment[:,1:], index=self.dates, columns=['open', 'close', 'high', 'low', 'volume'])
 
-print(hsma40.describe())
-print(lsma40.describe())
-print(ema15.describe())
+        # pandas DataFrame은 이런식으로 index 번호가 아닌, 위에서 columns 파라미터로 지정된 label로 각 column을 지정한다.
+        self.hsma40 = self.segment_pd['high'].rolling(40).mean()
+        self.lsma40 = self.segment_pd['low'].rolling(40).mean()
+        self.ema15 = self.segment_pd['close'].ewm(15).mean()
 
-for i in range(segment.shape[0]):
+        print(self.hsma40.describe())
+        print(self.lsma40.describe())
+        print(self.ema15.describe())
+
+    def draw(self, ax, i):
+        begin = i - required_lead
+        end = i + 50
+
+        open = self.segment[i,1]
+        close = self.segment[i,2]
+        center = (open + close) / 2
+        min_y = center - 5
+        max_y = center + 5
+
+        plt.xticks([self.dates[i]], rotation=0)
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
+        ax.set_ylim(bottom = min_y, top = max_y)
+
+        ax.add_patch(mask_h(plt, self.dates[i] - (30/(24*60)/2), self.dates[i] + (30/(24*60)/2), min_y, max_y, 'yellow', 1.0))
+
+        plot_day_summary_ohlc(ax, self.ohlc[begin:end])
+
+        ax.plot(self.hsma40[self.dates[begin]:self.dates[end]], color = 'blue', linewidth = 2, label='High, 40-Day SMA')
+        ax.plot(self.lsma40[self.dates[begin]:self.dates[end]], color = 'blue', linewidth = 2, label='Low, 40-Day SMA')
+        ax.plot(self.ema15[self.dates[begin]:self.dates[end]], color = 'red', linestyle='--', linewidth = 2, label='Close, 15-Day EMA')
+
+def get_candle(segment, i):
     ts = segment[i,0]
     open = segment[i,1]
     close = segment[i,2]
@@ -47,30 +71,21 @@ for i in range(segment.shape[0]):
     low = segment[i,4]
     volume = segment[i,5]
     movement_percent = (close / open - 1) * 100
+    return (ts, open, close, high, low, volume, movement_percent)
+
+segment = retrieve_polo('USDT_LTC', 30, '2020-08-10', '2020-08-16') # 요구된 봉 데이터를 Poloniex 거래소 API로 읽어와서 n x 6 크기의 numpy array로 만들어주는 함수
+ltc = Series(segment)
+
+for i in range(segment.shape[0]):
+    (ts, open, close, high, low, volume, movement_percent) = get_candle(segment, i)
 
     if i > required_lead and movement_percent > purge_percent_threshold:
         print('[{:05d}] {:s} {:7.3f} {:7.3f} {:7.3f} {:7.3f} {:10.3f} {:6.2f}%'.format(i, str(datetime.fromtimestamp(ts)), open, close, high, low, volume, movement_percent))
 
         if snapshots_count < max_snapshots:
 
-            begin = i - required_lead
-            end = i + 50
-
-            center = (open + close) / 2
-            min_y = center - 5
-            max_y = center + 5
-
-            ax = fig.add_subplot(gs_all[0, snapshots_count], ylim=(min_y, max_y))
-
-            plot_day_summary_ohlc(ax, ohlc[begin:end])
-            plt.xticks([dates[i]], rotation=0)
-            ax.add_patch(mask_h(plt, dates[i] - (30/(24*60)/2), dates[i] + (30/(24*60)/2), min_y, max_y, 'yellow', 1.0))
-            plot_day_summary_ohlc(ax, ohlc[begin:end])
-            ax.plot(hsma40[dates[begin]:dates[end]], color = 'blue', linewidth = 2, label='High, 40-Day SMA')
-            ax.plot(lsma40[dates[begin]:dates[end]], color = 'blue', linewidth = 2, label='Low, 40-Day SMA')
-            ax.plot(ema15[dates[begin]:dates[end]], color = 'red', linestyle='--', linewidth = 2, label='Close, 15-Day EMA')
-
-            ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
+            ax = fig.add_subplot(gs_all[0, snapshots_count])
+            ltc.draw(ax, i)
 
             snapshots_count += 1
 
